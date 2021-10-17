7a588dc18379efc5c562d4d3c58790f3f764214c
WinterP <winterp.dev@gmail.com>
2021-10-17 17:37:08 -0500

    Fizzbuzz Upload

--- JS Assignments/Fizzbuzz/script.js
+++ JS Assignments/Fizzbuzz/script.js
@@ -2,16 +2,12 @@ function fizzbuzz(end){
     for(var i=1; i <= end; i++) {
         if((i % 3 == 0) && (i % 5 == 0)) {
             console.log("Fizzbuzz");
+        } else if(i % 3 == 0) {
+            console.log("Fizz");
+        } else if(i % 5 == 0) {
+            console.log("Buzz");
         } else {
-            if(i % 3 == 0) {
-                console.log("Fizz");
-            } else {
-                if(i % 5 == 0) {
-                    console.log("Buzz");
-                } else {
-                    console.log(i);
-                }
-            }
+            console.log(i);
         }
     }
 };
